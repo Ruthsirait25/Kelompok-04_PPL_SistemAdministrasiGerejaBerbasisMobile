c620c183e72f0cf01557c25c0978b5799a843ba1
S05pan <sopranita08@gmail.com>
2023-12-01 20:46:54 +0700

    Update registrasi

--- app/src/main/java/com/example/hkbptarutung/registrasi/request/RegistrasiPemberkatanNikah.kt
+++ app/src/main/java/com/example/hkbptarutung/registrasi/request/RegistrasiPemberkatanNikah.kt
@@ -1,50 +1,58 @@
 package com.example.hkbptarutung.registrasi.request
 
+import android.view.View
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
-import androidx.viewbinding.ViewBinding
 import com.example.hkbptarutung.R
-import com.example.hkbptarutung.databinding.ActivityRegistrasiJemaatBaruBinding
+import com.example.hkbptarutung.databinding.ActivityRegistrasiPemberkatanNikahBinding
 import com.example.hkbptarutung.registrasi.RegistrasiPagePresenter
 import com.example.hkbptarutung.utils.FirebaseUtils
 import com.example.hkbptarutung.utils.PreferenceUtils
 import com.example.hkbptarutung.utils.setDatePicker
+import com.example.hkbptarutung.utils.showDatePicker
+import com.example.hkbptarutung.utils.toDateStr
 import com.example.hkbptarutung.utils.value
+import com.google.firebase.Timestamp
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.Date
+import java.util.Locale
 
-class RegistrasiJemaatBaru : BaseRequestPage() {
+class RegistrasiPemberkatanNikah : BaseRequestPage() {
 
     val presenter = RegistrasiPagePresenter(this)
 
     override fun setupListener() {
         super.setupListener()
         findViewById<TextView>(R.id.edt_tanggal_lahir).setDatePicker()
+        findViewById<TextView>(R.id.edt_tanggal_martumpol).setDatePicker()
     }
 
     override fun submit() {
         if (PreferenceUtils.isAdmin(this)) {
             docId()?.apply {
-                presenter.approveJemaat(this)
+                presenter.approveNikah(this)
             }
             return
         }
-        presenter.submitJemaatBaru(
+        presenter.submitNikah(
             nik = findViewById<EditText>(R.id.edt_nik)?.value(),
             fullName = findViewById<EditText>(R.id.edt_nama_jemaat).value(),
             birthDate = findViewById<TextView>(R.id.edt_tanggal_lahir).value(),
             birthPlace = findViewById<EditText>(R.id.edt_tempat_lahir).value(),
-            gender = findViewById<Spinner>(R.id.sp_jekel).selectedItem.toString(),
+            martumpolPlace = findViewById<EditText>(R.id.edt_tempat_martumpol).value(),
+            martumpolDate = findViewById<TextView>(R.id.edt_tanggal_martumpol).value(),
             bloodType = findViewById<EditText>(R.id.edt_goldar).value(),
             address = findViewById<EditText>(R.id.edt_alamat).value(),
             phone = findViewById<EditText>(R.id.edt_nomorHP).value(),
-            gereja = findViewById<EditText>(R.id.edt_gereja).value()
         )
     }
 
     override fun reject() {
         if (PreferenceUtils.isAdmin(this)) {
             docId()?.apply {
-                presenter.rejectJemaat(this)
+                presenter.rejectNikah(this)
             }
             return
         }
@@ -59,15 +67,14 @@ class RegistrasiJemaatBaru : BaseRequestPage() {
             findViewById<EditText>(R.id.edt_nama_jemaat).setText("${it["fullName"]}")
             findViewById<TextView>(R.id.edt_tanggal_lahir).setText("${it["birthDate"]}")
             findViewById<EditText>(R.id.edt_tempat_lahir).setText("${it["birthPlace"]}")
-            val gender = if (it["gender"] == getString(R.string.female)) 1 else 0
-            findViewById<Spinner>(R.id.sp_jekel).setSelection(gender)
             findViewById<EditText>(R.id.edt_goldar).setText("${it["bloodType"]}")
             findViewById<EditText>(R.id.edt_alamat).setText("${it["address"]}")
             findViewById<EditText>(R.id.edt_nomorHP).setText("${it["phone"]}")
-            findViewById<EditText>(R.id.edt_gereja).setText("${it["originChurch"]}")
+            findViewById<EditText>(R.id.edt_tempat_martumpol).setText("${it["martumpol_place"]?:""}")
+            findViewById<TextView>(R.id.edt_tanggal_martumpol).setText("${it["martumpol_date"]?:""}")
         }
     }
 
-    override val binding: ViewBinding
-        get() = ActivityRegistrasiJemaatBaruBinding.inflate(layoutInflater)
+    override val binding: ActivityRegistrasiPemberkatanNikahBinding
+        get() = ActivityRegistrasiPemberkatanNikahBinding.inflate(layoutInflater)
 }
